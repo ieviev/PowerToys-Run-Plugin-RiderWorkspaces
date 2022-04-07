9e485e88d88b023e0681108d4b47e804ff310c6b
nr-4 <36763595+nr-4@users.noreply.github.com>
2022-04-07 23:51:40 +0300

    listener upd

--- filelistener.fsx
+++ filelistener.fsx
@@ -1,28 +1,21 @@
-#load @"D:\git\scripts\commonimports.fsx" 
 open System
-open System.IO
-open System.Text
-open FSharp.Data
-open FSharpPlus
-open FSharpPlus.Data
-open Fs
-open Fs.Utils
-
-open System.Threading
-open System
+open System.Diagnostics
 
 let watcher = new System.IO.FileSystemWatcher(".","*.fs")
 let sub fn = watcher.Changed.Subscribe(fun f -> fn f)
 watcher.EnableRaisingEvents <- true
 watcher.IncludeSubdirectories <- true
 
-let mutable nextCall = DateTimeOffset.Now // limit to one per 10s
+let mutable nextCall = DateTimeOffset.Now // limit to one build per 10s
 
 sub (fun f -> 
     if DateTimeOffset.Now < nextCall then () else
     nextCall <- nextCall.AddSeconds(10)
-    printfn $"sub:%A{f.Name}"
-    Process.createPowerShell "pwsh ./postbuild.ps1" |> ignore
+    //printfn $"sub:%A{f.Name}"
+    let psstartinfo = ProcessStartInfo("powershell","./postbuild.ps1")
+    use proc = new Process(StartInfo=psstartinfo)
+    proc.Start() |> ignore
+    proc.WaitForExit()
 )
 
 let resetevent = new System.Threading.ManualResetEvent(false)
--- postbuild.ps1
+++ postbuild.ps1
@@ -1,8 +1,7 @@
-# dotnet publish -c Release -o publish -r win-x64 --framework net48
 dotnet publish -c Release -o publish -r win-x64 --framework net6.0-windows
 
 $localpublish = "publish"
-$out = "C:\Users\kast\AppData\Local\Microsoft\PowerToys\PowerToys Run\Plugins\publish"
+$out = "$env:LOCALAPPDATA\Microsoft\PowerToys\PowerToys Run\Plugins\publish"
 
 Copy-Item -Recurse $localpublish\* $out -Force
 
@@ -10,10 +9,6 @@ $currlocation = Get-Location
 Set-Location $out
 
 Stop-Process -n PowerToys
-
-# C:\Users\kast\.nuget\packages\ilmerge\3.0.41\tools\net452\ILMerge.exe /out:RiderWorkspaces.dll `
-# FSharp.Core.dll `
-# PowerToys.Run.Plugin.RiderWorkspaces.dll 
 
 ILRepack.exe /out:RiderWorkspaces.dll `
 FSharp.Core.dll `
